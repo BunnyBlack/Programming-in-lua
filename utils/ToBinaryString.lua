d4172db60a2709c1a586311962d5fb933489c0c9
qw <xyy6836@126.com>
2020-08-29 20:50:37 +0800

    create: /utils/ToBinaryString

---
+++ utils/ToBinaryString.lua
@@ -0,0 +1,13 @@
+--- 用于把给定的整形数转换成二进制形式的字符串
+---@return string
+---@param n number
+function ToBinaryString(n)
+    local t = {}
+    while n > 0 do
+        table.insert(t, n & 1)
+        n = n >> 1
+    end
+
+    return string.reverse(table.concat(t))
+end
+
