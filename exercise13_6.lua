6de1713b8340a5e933932c846b473f44daf566b0
qw <xyy6836@126.com>
2020-09-29 18:19:00 +0800

    complete 'Todo' in chapter 13 and chapter 14

--- exercise13_6.lua
+++ exercise13_6.lua
@@ -1,1 +1,28 @@
---todo bitarray
+BitArray = {
+    array = {},
+
+    setBit = function(self, n, v)
+        self.array[n] = v
+    end,
+    testBit = function(self, n)
+        return self.array[n]
+    end
+}
+
+function BitArray:new(n)
+    local o = {array = {}}
+    for i = 1, n do
+        table.insert(o.array, false)
+    end
+    self.__index = self
+    setmetatable(o, self)
+    return o
+end
+
+local ba = BitArray:new(3)
+print(ba.testBit(ba,1))
+print(ba.testBit(ba,2))
+print(ba.testBit(ba,3))
+print(ba.testBit(ba,4))
+ba.setBit(ba,1,true)
+print(ba.testBit(ba,1))
--- exercise14_2.lua
+++ exercise14_2.lua
@@ -1,1 +1,58 @@
---todo Queue
+DoubleEndedQueue = {
+    list = {first = 0, last = 0},
+    pushFirst = function(self, v)
+        if(self.list.first == 0) then
+            self.list.first = 1
+            self.list[self.list.first] = v
+        else
+            self.list.first = self.list.first + 1
+            self.list[self.list.first] = v
+        end
+    end,
+    pushLast = function(self, v)
+        if(self.list.last == 0) then
+            self.list.last = -1
+            self.list[self.list.last] = v
+        else
+            self.list.last = self.list.last - 1
+            self.list[self.list.last] = v
+        end
+    end,
+    popFirst = function(self)
+        if(self.list.first == 0) then
+            error("stack overflow",2)
+        else
+            local temp = self.list[self.list.first]
+            self.list[self.list.first] = nil
+            self.list.first = self.list.first - 1
+            return temp
+        end
+    end,
+    popLast = function(self)
+        if(self.list.last == 0) then
+            error("stack overflow", 2)
+        else
+            local temp = self.list[self.list.last]
+            self.list[self.list.last] = nil
+            self.list.last = self.list.last + 1
+            return temp
+        end
+    end
+}
+
+function DoubleEndedQueue:new(o)
+    o = o or {}
+    self.__index = self
+    setmetatable(o, self)
+    return o
+end
+
+local deq = DoubleEndedQueue:new()
+deq:pushFirst(1)
+deq:pushFirst(2)
+deq:pushLast(3)
+deq:pushLast(4)
+print(deq:popFirst())
+print(deq:popFirst())
+print(deq:popLast())
+print(deq:popLast())
